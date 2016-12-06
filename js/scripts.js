986502ee17fe8a6bc429bb7e7dd3a15ab88eaff6
magnus udbjorg <magnus@udbjorg.net>
2016-12-06 15:51:51 +0100

    ha ha ha ha

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,60 @@
-(function($) {
+var player;
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'E8gmARGvPlI',
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+var startTimer = function (duration) {
+    var timer = duration;
+
+    var interval = setInterval(function () {
+        var display = document.querySelector('#timer');
+        display.textContent = timer;
+        if (--timer < 0) {
+            clearInterval(interval);
+            $(".media-image").fadeIn();
+            player.playVideo();
+            player.unMute();
+            var slider = $('.lyrics');
+            slider.addClass("animate");
+            slider.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+                function (e) {
+                    slider.removeClass('animate');
+                    slider.addClass("finished");
+                });
+
+        }
+
+    }, 1000);
+};
+var onPlayerReady = function () {
+    player.mute()
+    player.stopVideo();
+    startTimer(10);
+};
+
+(function ($) {
     "use strict";
 
     $('body').scrollspy({
         target: '.navbar-fixed-top',
-        offset: 60
+        offset: 0
     });
 
     $('#topNav').affix({
@@ -11,24 +62,16 @@
             top: 200
         }
     });
-    
+
     new WOW().init();
-    
-    $('a.page-scroll').bind('click', function(event) {
+
+    $('a.page-scroll').bind('click', function (event) {
         var $ele = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($ele.attr('href')).offset().top - 60)
+            scrollTop: ($($ele.attr('href')).offset().top)
         }, 1450, 'easeInOutExpo');
         event.preventDefault();
     });
-    
-    $('.navbar-collapse ul li a').click(function() {
-        /* always close responsive nav after click */
-        $('.navbar-toggle:visible').click();
-    });
-
-    $('#galleryModal').on('show.bs.modal', function (e) {
-       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
-    });
 
 })(jQuery);
+
